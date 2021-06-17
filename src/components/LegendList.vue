17db859e69776a775400694ecaae62254ef1b7dc
Bob Torgerson <rltorgerson@alaska.edu>
2021-06-17 15:16:49 -0800

    Updated order of legends for all layers.

--- src/components/LegendList.vue
+++ src/components/LegendList.vue
@@ -2,16 +2,16 @@
 	<div>
 		<legend-item id="fires"></legend-item>
 		<legend-item id="viirs"></legend-item>
+		<legend-item id="postgis_lightning"></legend-item>
 		<legend-item id="spruceadj_3338"></legend-item>
 		<legend-item id="snow_cover_3338"></legend-item>
-		<legend-item id="postgis_lightning"></legend-item>
+		<legend-item id="alaska_landcover_2015"></legend-item>
+		<legend-item id="gridded_lightning"></legend-item>
 		<legend-item id="alaska_wildfires"></legend-item>
 		<legend-item id="historical_fire_perimiters"></legend-item>
-		<legend-item id="gridded_lightning"></legend-item>
 		<legend-item
 			id="alfresco_relative_flammability_NCAR-CCSM4_rcp85_2000_2099"
 		></legend-item>
-		<legend-item id="alaska_landcover_2015"></legend-item>
 	</div>
 </template>
 
