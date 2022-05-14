4e0a53baf56734a3679b37cd508314e04f1367ba
Craig Stephenson <crstephenson@alaska.edu>
2022-05-13 17:17:15 -0800

    Prevent 2022 wildfire legend text from overlapping icon table.

--- src/layers.js
+++ src/layers.js
@@ -7,6 +7,7 @@ export default [
     title: "2022 Wildfires",
     local: true,
     visible: true,
+    legendClassOverride: "is-one-third",
     legend: `<table class="alaska-wildfires-legend active-fires">
       <tr><td><img src="` + require('@/assets/fire-perimeter.png') + `"/></td><td class="fire-text">Active Fire Perimeters</td></tr>
       <tr><td><img src="` + require('@/assets/large-fire.png') + `"/></td><td class="fire-text">Large Fire</td></tr>
