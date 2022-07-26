427597442ee587d39fd7c0d14871773007e023d0
Bruce Crevensten <bruce.crevensten@gmail.com>
2022-07-26 15:43:01 -0700

    Adds buefy, message linking to Fire Tally

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 
+import { Message } from 'buefy'
+import 'buefy/dist/buefy.css'
+Vue.use(Message)
+
 require("@/assets/main.scss");
 
 Vue.config.productionTip = false;
