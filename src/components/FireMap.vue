312456a51871570bb9201455c8fdf25af2db6d3d
Bruce Crevensten <bruce.crevensten@gmail.com>
2024-07-02 11:36:38 -0700

    increase max zoom level one step

--- src/components/FireMap.vue
+++ src/components/FireMap.vue
@@ -171,7 +171,7 @@ export default {
       mapOptions: {
         zoom: 1,
         minZoom: 1,
-        maxZoom: 5,
+        maxZoom: 6,
         center: [65, -152.5],
       },
       baseLayerOptions: {
