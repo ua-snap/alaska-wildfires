16bcd52443346afc9c4ad034c6ef2e9d82fce647
Bob Torgerson <rltorgerson@alaska.edu>
2025-03-12 12:47:22 -0800

    Updates development and production environments. Development uses July 24, 2024 for its available data.

--- src/components/FireMap.vue
+++ src/components/FireMap.vue
@@ -40,8 +40,6 @@ Object.defineProperty(Vue.prototype, "$L", { value: L });
 Object.defineProperty(Vue.prototype, "$axios", { value: axios });
 Object.defineProperty(Vue.prototype, "$moment", { value: moment });
 
-const wfsUrl = "https://gs.earthmaps.io/geoserver/wfs";
-
 // Wire in two listeners that will keep track of open
 // HTTP requests.
 Vue.prototype.$axios.interceptors.request.use(
@@ -78,6 +76,10 @@ var fireLayerGroup;
 var viirsLayerGroup;
 var purpleAirMarkers;
 var purpleAirLayerGroup;
+
+console.log("WMS URL:", process.env.VUE_APP_GEOSERVER_WMS_URL);
+console.log("WFS URL:", process.env.VUE_APP_GEOSERVER_WFS_URL);
+console.log("Active:", process.env.VUE_APP_ACTIVE);
 
 // Current time zone offset (used in parseDate below).
 var offset = new Date().getTimezoneOffset();
@@ -227,7 +229,7 @@ export default {
 
       return new Promise((resolve) => {
         this.$axios
-          .get(wfsUrl, { params })
+          .get(process.env.VUE_APP_GEOSERVER_WFS_URL, { params })
           .then((response) => {
             if (response.data) {
               // Process the WFS data
@@ -363,7 +365,7 @@ export default {
 
       return new Promise((resolve) => {
         this.$axios
-          .get(wfsUrl, { params })
+          .get(process.env.VUE_APP_GEOSERVER_WFS_URL, { params })
           .then((response) => {
             if (response.data) {
               // Process the WFS data
@@ -422,8 +424,12 @@ export default {
       };
 
       return Promise.all([
-        this.$axios.get(wfsUrl, { params: pointParams }),
-        this.$axios.get(wfsUrl, { params: polygonParams }),
+        this.$axios.get(process.env.VUE_APP_GEOSERVER_WFS_URL, {
+          params: pointParams,
+        }),
+        this.$axios.get(process.env.VUE_APP_GEOSERVER_WFS_URL, {
+          params: polygonParams,
+        }),
       ])
         .then((responses) => {
           let fireCount = 0;
